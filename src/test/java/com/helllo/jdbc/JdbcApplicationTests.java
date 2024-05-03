d0d03e1b42d42c3d04db841203d5db494e9698c1
yongchann <yongcgp@naver.com>
2024-05-03 15:21:20 +0900

    Initial commit

---
+++ src/test/java/com/helllo/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helllo.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
