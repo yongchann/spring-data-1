20fadea34344b1cd0edb756be4ad7dfb4a95ed07
yongchann <yongcgp@naver.com>
2024-05-07 23:48:40 +0900

    [ch3] MemberServiceV2 추가 - 트랜잭션 처리를 위해 Service 레이어에서 DataSource 를 의존함 -> 트랜잭션 관련 코드 추가

---
+++ src/test/java/com/helllo/jdbc/service/MemberServiceV2Test.java
@@ -0,0 +1,79 @@
+package com.helllo.jdbc.service;
+
+import com.helllo.jdbc.domain.Member;
+import com.helllo.jdbc.repository.MemberRepositoryV1;
+import com.helllo.jdbc.repository.MemberRepositoryV2;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static com.helllo.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 트랜잭션 - 커넥션 파라미터 전달 방식
+ */
+@Slf4j
+class MemberServiceV2Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV2 memberRepository;
+    private MemberServiceV2 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV2(dataSource);
+        memberService = new MemberServiceV2(dataSource, memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.clear();
+    }
+
+    @Test
+    void 정상_이체() throws SQLException {
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        memberService.accountTransfer(MEMBER_A, MEMBER_B, 5000);
+
+        Member findMemberA = memberRepository.findById(MEMBER_A);
+        Member findMemberB = memberRepository.findById(MEMBER_B);
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(5000);
+        Assertions.assertThat(findMemberB.getMoney()).isEqualTo(15000);
+    }
+
+    @Test
+    void 이체_오류() throws SQLException {
+        //given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        //when
+        log.info("START TX");
+        Assertions.assertThatThrownBy(() -> memberService.accountTransfer(MEMBER_A, MEMBER_EX, 5000))
+                .isInstanceOf(IllegalStateException.class);
+
+        //then
+        Member findMemberA = memberRepository.findById(MEMBER_A);
+        Member findMemberEx = memberRepository.findById(MEMBER_EX);
+
+        Assertions.assertThat(findMemberA.getMoney()).isEqualTo(10000); // A의 잔고만 줄어들어 비즈니스 로직의 원자성이 깨짐
+        Assertions.assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+    }
+
+}
