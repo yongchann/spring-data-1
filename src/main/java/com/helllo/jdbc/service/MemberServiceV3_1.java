5221ecd24db0b1b971e818841aafa009d942f56c
yongchann <yongcgp@naver.com>
2024-05-08 14:44:55 +0900

    [ch4] MemberServiceV3_1 추가 - 서비스 레이어에서 PlatformTransactionManager 를 사용해 트랜잭션 제어
    - 서비스의 JDBC DataSource 의존성을 제거함
    - 스프링의 PlatformTransactionManager 를 의존하고 이 인터페이스는 getTransaction(), commit(), rollback() 을 가짐. 서비스 사용 시 구현체를 주입하여 다형성 달성 가능 (DataSoureceTransactionManager, JpaTransactionManager 등)

---
+++ src/main/java/com/helllo/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,52 @@
+package com.helllo.jdbc.service;
+
+import com.helllo.jdbc.domain.Member;
+import com.helllo.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+// SQLException 을 제외한 JDBC 의존성 제거됨
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    //    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws IllegalStateException {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+//            con.setAutoCommit(false); // 트랜잭션 시작
+            // 비즈니스 로직
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new IllegalStateException(e);
+        } // finally 구문을 transactionManager 에서 처리함
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
