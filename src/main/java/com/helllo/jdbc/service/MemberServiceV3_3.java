a5d4546a4628e658fd449f39809cacea9ea53245
yongchann <yongcgp@naver.com>
2024-05-08 16:12:31 +0900

    [ch4] MemberServiceV3_3 추가 - Spring AOP 적용 @Transactional

---
+++ src/main/java/com/helllo/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,39 @@
+package com.helllo.jdbc.service;
+
+import com.helllo.jdbc.domain.Member;
+import com.helllo.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
